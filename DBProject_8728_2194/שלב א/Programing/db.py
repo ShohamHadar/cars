8bfce9e51396a631d875e483467e80cae512bed0
Shoham <ronitm2233@gmail.com>
2026-06-02 12:04:04 +0300

    Update db.py

--- DBProject_8728_2194/שלב א/Programing/db.py
+++ DBProject_8728_2194/שלב א/Programing/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, text
 
-DATABASE_URL = "postgresql://ori:ori@localhost:5432/carsDB"
+DATABASE_URL = "postgresql://shoham:shoham@localhost:5432/carsDB"
 
 engine = create_engine(DATABASE_URL)
